3985a193d0b6b7170bf629d1f8885deb2b7ce364
Joaquinfr87 <vader87joaquin@gmail.com>
2025-03-26 11:14:17 -0400

    Prueba

--- Fractales-ProyectoFinal/Form1.cs
+++ Fractales-ProyectoFinal/Form1.cs
@@ -2,9 +2,21 @@ namespace Fractales_ProyectoFinal
 {
     public partial class Form1 : Form
     {
+        int count = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            label1.Text = count.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
